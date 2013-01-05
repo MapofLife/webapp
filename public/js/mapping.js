2d92b65d1f48591d63db6c3ba0b4fdd8b289079b
Sander Pick <sanderpick@gmail.com>
2013-01-04 20:58:01 -0800

    redo google lib loading and add the visualization api

--- public/js/mapping.js
+++ public/js/mapping.js
@@ -1,15 +1,29 @@
 /**
- * This module loads the Google Maps API and then the CartoDB API, putting both
- * into global scope as "google" and "CartoDB", respectively. 
+ * This module loads the Google Maps API and the Google Visualization API,
+ * and then the CartoDB API, putting both into global scope
+ * as "google" and "CartoDB", respectively. 
  */
 
 define([
   // dependencies
   'jQuery',
-  'Underscore',
-  'libs/google/maps/load'
-], function ($, _, loadGoogleMaps) {
-  return {   
+  'Underscore'
+], function ($, _) {
+  
+  var libs = [
+    {
+      name: 'maps',
+      version: '3',
+      options: { other_params: 'sensor=false' }
+    },
+    {
+      name: 'visualization',
+      version: '1',
+      options: { packages: ['corechart'] }
+    }
+  ];
+
+  return {
 
     /**
      * Loads the Google Maps API and then the CartoDB API. Fires the callback
@@ -18,11 +32,19 @@ define([
      * @param cb The callback function.
      */
     init: function (cb) {
-      loadGoogleMaps({
-        key: 'AIzaSyDJdVhfQhecwp0ngAGzN9zwqak8FaEkSTA',
-        geometry: false,
-      }).done(function () {
+
+      // After each lib is loaded, get the cartodb lib.
+      var done = _.after(libs.length, function () {
         require(['libs/cartodb/cartodb-gmapsv3'], cb);
+      });
+
+      // Load the jsapi and then grab each lib.
+      require(['https://www.google.com/jsapi?callback=?'
+              + '&key=AIzaSyDJdVhfQhecwp0ngAGzN9zwqak8FaEkSTA'], function () {        
+        _.each(libs, function (lib) {
+          google.load(lib.name, lib.version,
+                    _.extend(lib.options, { callback: done }));
+        });
       });
     }
   }
